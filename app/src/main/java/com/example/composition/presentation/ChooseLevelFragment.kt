836a573032aac46d4b552f50b25672030274c56d
Nikolai Lisenko <kollysenko@gmail.com>
2022-03-21 00:27:45 +0500

    added jetpack navigation

--- app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/composition/presentation/ChooseLevelFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.composition.R
+import androidx.navigation.fragment.findNavController
 import com.example.composition.databinding.FragmentChooseLevelBinding
 import com.example.composition.domain.entity.Level
 import java.lang.RuntimeException
@@ -28,7 +28,7 @@ class ChooseLevelFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
-           buttonLevelTest.setOnClickListener {
+            buttonLevelTest.setOnClickListener {
                 launchGameFragment(Level.TEST)
             }
             buttonLevelEasy.setOnClickListener {
@@ -45,10 +45,9 @@ class ChooseLevelFragment : Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFragment.newInstance(level))
-            .addToBackStack(GameFragment.NAME)
-            .commit()
+        findNavController().navigate(
+            ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(level)
+        )
     }
 
     override fun onDestroyView() {
@@ -56,12 +55,5 @@ class ChooseLevelFragment : Fragment() {
         _binding = null
     }
 
-    companion object {
 
-        const val NAME = "ChooseLevelFragment"
-
-        fun newInstance(): ChooseLevelFragment {
-            return ChooseLevelFragment()
-        }
-    }
 }
